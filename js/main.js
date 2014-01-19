111c01854da0f6679f80b1813e21c8c961eb7a13
Jeff Davis <jtdavis247>
2014-01-18 23:25:25 -0700

    adds textbox resizing as well as character count

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(function(){
+
+	$('textarea').click(function(){
+		var that = $(this);
+			that.addClass('expand');
+			$('#tweet-controls').show();
+		});
+	$('textarea').on('blur', function(){
+		var that  = $(this);
+		that.css('height', '40px');
+		$('#tweet-controls').hide();
+	});
+
+	$('textarea').keydown(function() {
+		var len           = $(this).val().length,
+			remainingChar = 139 -len;
+
+		if (remainingChar <= 10) {
+			$(this).css('color', 'red');
+		}
+		$('#char-count').html(remainingChar);
+		if (remainingChar <= 0) {
+			$('.button#tweet-submit').attr('disabled', true)
+		}
+		else if (remainingChar >= 1) {
+			$('.button#tweet-submit').attr('disabled', false);
+		}
+	});
+
+});
